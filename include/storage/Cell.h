d9f4266463ee2ec5afac6792465c2234faeadd51
w3rr0 <konradmateja65@gmail.com>
2026-04-04 19:35:54 +0200

    add Cell.h

---
+++ include/storage/Cell.h
@@ -0,0 +1,43 @@
+#ifndef CELL_H
+#define CELL_H
+
+#include <variant>
+#include <string>
+#include <iostream>
+
+class Cell {
+public:
+    using ValueType = std::variant<std::monostate, int, double, std::string, bool>;
+    enum Type {
+        NULL_TYPE, INT, DOUBLE, TEXT, BOOL
+    };
+
+    // Constructors
+    Cell() : value(std::monostate{}) {};
+    explicit Cell(int v) : value(v) {}
+    explicit Cell(double v) : value(v) {}
+    explicit Cell(std::string v) : value(std::move(v)) {}
+    explicit Cell(bool v) : value(v) {}
+
+    // Getters
+    Type getType() const;
+
+    template<typename T>
+    const T& as() const { return std::get<T>(value); }
+
+    // Comparisons
+    bool operator==(const Cell& other) const;
+    bool operator<(const Cell& other) const;
+    bool operator!=(const Cell& other) const { return !(*this == other); }
+    bool operator>(const Cell& other) const  { return other < *this; }
+    bool operator<=(const Cell& other) const { return !(*this > other); }
+    bool operator>=(const Cell& other) const { return !(*this < other); }
+
+    // Display
+    friend std::ostream& operator<<(std::ostream& os, const Cell& cell);
+
+private:
+    ValueType value;
+};
+
+#endif
